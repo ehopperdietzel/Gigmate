32deeec061a89ad5bde4980a8e9cae11b71cad0d
Diego Sandoval <sandov@protonmail.com>
2016-04-17 20:00:08 -0300

    Empezando a hacer el registro de usuarios nuevos

---
+++ register.php
@@ -0,0 +1,9 @@
+<?php 
+include_once("connection.php");
+/*falta crear la conexion con la DB,
+para ello debemos primero decidir la organizacion
+de las tablas y los campos*/
+
+include_once("header.php");
+?>
+
