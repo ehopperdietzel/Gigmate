3d1d054d779e21be765fd14ca20481d433bcb335
Diego Sandoval <sandov@protonmail.com>
2016-04-17 20:28:43 -0300

    Agregados los primeros archivos para realizar el registro

---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bienvenido a Gigmate</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/gigmate.css">
+    <link rel="stylesheet" href="css/city-autocomplete.css">
+
+    <!-- Google Maps' Places API -->
+    <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?libraries=places&language=en"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+  <nav class="navbar navbar-default topbar">
+    <div class="container-fluid">
+     
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#"><img class="logo" alt="Gigmate" src="img/logo.png"></a>
+      </div>
+
+      <form class="navbar-form navbar-right" role="search" style="padding-top: 35px;">
+        <div class="form-group">
+          <input type="text" class="form-control" name="username" placeholder="Correo o usuario">
+        </div>
+        <div class="form-group">
+          <input type="text" class="form-control" name="password" placeholder="Contraseña">
+        </div>
+        <button type="submit" class="btn btn-default">Iniciar</button>
+      </form>
+
+    </div>
+  </nav>
